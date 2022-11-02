03306b88971f0307698bb42c35cd56c71947c952
mujaffar09 <110367256+mujaffar09@users.noreply.github.com>
2022-11-03 00:23:36 +0530

    Add files via upload

---
+++ Button.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+function Button() {
+  const [value,setValue]=useState(10)
+
+  const setToValue =(newValue) =>()=> {
+    console.log('value now', newValue)
+    setValue(newValue)
+  }
+  return (
+    <div>
+      {value}
+      <button onClick={setToValue(1000)}>Thousand </button> 
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment </button>  
+    </div>
+  )
+}
+
+export default Button
+
