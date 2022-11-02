b31728d4b5d7d5ed418718880c10397df00d1aee
mujaffar09 <110367256+mujaffar09@users.noreply.github.com>
2022-11-03 00:30:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Counter from './components/Counter'
+import Button from './components/Button';
+
+function App() {
+  return (
+    <div className="App">
+       <Counter />
+       <Button />
+    </div>
+  );
+}
+
+export default App;
